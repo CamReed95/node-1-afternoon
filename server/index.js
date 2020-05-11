8e7d76c6b4207248642a44e6930eb03fb130a405
Cameron Reed <cameronreedadjusting@gmail.com>
2020-05-11 11:19:53 -0700

    Completed afternoon

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express')
-const products = require('../products.json')
-const getProducts = require('../getProducts')
-
-
 const app = express()
+app.use(express.json())
+const port = 4000
 
-const port = 3001
+const products = require('../products.json')
 
-app.get(`/api/products`, getProducts)
-app.get(`/api/product/:id`, getProduct)
+const getProducts = require('./getProducts')
+
+const getProduct = require('./getProduct')
 
 
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`)
-})
+app.listen(port, ()=>console.log(`server listening on port ${port}`))
 
+app.get('/api/products', getProducts)
+
+app.get('/api/product/:id', getProduct)
